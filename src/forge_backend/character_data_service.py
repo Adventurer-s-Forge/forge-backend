4aefb390da85bb17177166f09e16b3256149c126
Jafreen Rahman <jpr6509@psu.edu>
2026-09-13 16:19:44 -0400

    Commented out an import

--- src/forge_backend/character_data_service.py
+++ src/forge_backend/character_data_service.py
@@ -1,4 +1,4 @@
-import requests
+# import requests
 from forge_backend.storage import get_redis, list_reference_records
 
 # This is the getter methods to grab information from the API or the database
