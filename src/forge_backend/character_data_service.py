0c3c9c1bdc41b8ad0d4bb466c1642d02b6038872
Jafreen Rahman <jpr6509@psu.edu>
2026-09-13 15:18:24 -0400

    modified the getter so that exclusively retrieves data from the redis

--- src/forge_backend/character_data_service.py
+++ src/forge_backend/character_data_service.py
@@ -9,19 +9,21 @@ class CharacterDataService:
         self.redis_client = get_redis()
 
     def get_races(self):
-        try:
-            response = requests.get("https://api.open5e.com/v1/races/", timeout=5)
-            response.raise_for_status()
-            response = response.json()
-        except requests.exceptions.RequestException:
-            response =  list_reference_records(self.redis_client, "race")
+        # try:
+        #     response = requests.get("https://api.open5e.com/v1/races/", timeout=5)
+        #     response.raise_for_status()
+        #     response = response.json()
+        # except requests.exceptions.RequestException:
+        #     response =  list_reference_records(self.redis_client, "race")
+        response =  list_reference_records(self.redis_client, "race")
         return response
 
     def get_classes(self):
-        try:
-            response = requests.get("https://api.open5e.com/v1/classes/", timeout=5)
-            response.raise_for_status()
-            response = response.json()
-        except requests.exceptions.RequestException:
-            response = list_reference_records(self.redis_client, "class")
+        # try:
+        #     response = requests.get("https://api.open5e.com/v1/classes/", timeout=5)
+        #     response.raise_for_status()
+        #     response = response.json()
+        # except requests.exceptions.RequestException:
+        #     response = list_reference_records(self.redis_client, "class")
+        response = list_reference_records(self.redis_client, "class")
         return response
