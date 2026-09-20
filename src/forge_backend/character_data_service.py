6849918083125976fee61778b49a3eb62b8896d1
Jafreen Rahman <jpr6509@psu.edu>
2026-09-19 22:55:40 -0400

    added singular getter methods

--- src/forge_backend/character_data_service.py
+++ src/forge_backend/character_data_service.py
@@ -1,5 +1,5 @@
 # import requests
-from forge_backend.storage import get_redis, list_reference_records
+from forge_backend.storage import get_redis, list_reference_records, get_reference
 
 
 # This is the getter methods to grab information from the API or the database
@@ -9,33 +9,36 @@ class CharacterDataService:
     def __init__(self):
         self.redis_client = get_redis()
 
+    def get_race(self, race):
+        return get_reference(self.redis_client, "race", race)
+
     def get_races(self):
-        # try:
-        #     response = requests.get("https://api.open5e.com/v1/races/", timeout=5)
-        #     response.raise_for_status()
-        #     response = response.json()
-        # except requests.exceptions.RequestException:
-        #     response =  list_reference_records(self.redis_client, "race")
         response =  list_reference_records(self.redis_client, "race")
         return response
 
+    def get_class(self, clas):
+        return get_reference(self.redis_client, "class", clas)
+
     def get_classes(self):
-        # try:
-        #     response = requests.get("https://api.open5e.com/v1/classes/", timeout=5)
-        #     response.raise_for_status()
-        #     response = response.json()
-        # except requests.exceptions.RequestException:
-        #     response = list_reference_records(self.redis_client, "class")
         response = list_reference_records(self.redis_client, "class")
         return response
+
+    def get_background(self, bg):
+        return get_reference(self.redis_client, "background", bg)
 
     def get_backgrounds(self):
         response = list_reference_records(self.redis_client, "background")
         return response
 
+    def get_item(self, item):
+        return get_reference(self.redis_client, "item", item)
+
     def get_items(self):
         response = list_reference_records(self.redis_client, "item")
         return response
+
+    def get_spell(self, spell):
+        return get_reference(self.redis_client, "spell", spell)
 
     def get_spells(self):
         response = list_reference_records(self.redis_client, "spell")
